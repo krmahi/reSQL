0ac512157a25711a7928451a2e0908925b791ddf
krmahi <mahesh.kr.2277@gmail.com>
2025-04-11 14:01:13 +0530

    && table

--- basics/table.sql
+++ basics/table.sql
@@ -21,3 +21,29 @@ SELECT
     *
 FROM
     students;
+
+DROP TABLE students;
+
+CREATE TABLE
+    students (rollno INT primary key, name varchar(50));
+
+SELECT
+    *
+FROM
+    students
+INSERT INTO
+    students
+values
+    (101, "mahi"),
+    (102, "ruhi"),
+    (103, "shradha");
+
+INSERT INTO
+    students (rollno, name)
+values
+    (104, "rohanni");
+
+INSERT INTO
+    students (name, rollno)
+values
+    ("suhani", 105);
